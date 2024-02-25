bb5e445b4bccbab504cbb3d5b38deaf8c90ee800
Jung su Lee <61855928+leejungsu1@users.noreply.github.com>
2024-02-25 14:32:37 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Button from "./Button";
+
+function Hello() {
+  useEffect(() => {
+    console.log("created");
+    return () => console.log("destoryed");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  const onClickHide = () => setShowing((prev) => !prev);
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("i run when 'counter & keyword' changes");
+  }, [counter, keyword]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={onChange} type="text" placeholder="Search here" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClickHide}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
